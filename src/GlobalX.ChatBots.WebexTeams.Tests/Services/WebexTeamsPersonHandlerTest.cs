fa9454dbb8d63d0b039287bff1b33aa68c2af53d
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2019-07-17 08:42:21 +1000

    test: add tests for webex teams person handler

---
+++ src/GlobalX.ChatBots.WebexTeams.Tests/Services/WebexTeamsPersonHandlerTest.cs
@@ -0,0 +1,82 @@
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using GlobalX.ChatBots.WebexTeams.Mappers;
+using GlobalX.ChatBots.WebexTeams.Mappers.Profiles;
+using GlobalX.ChatBots.WebexTeams.Services;
+using GlobalX.ChatBots.WebexTeams.Tests.TestData;
+using NSubstitute;
+using Shouldly;
+using TestStack.BDDfy;
+using Xunit;
+using GlobalXPerson = GlobalX.ChatBots.Core.People.Person;
+using WebexTeamsPerson = GlobalX.ChatBots.WebexTeams.Models.Person;
+
+namespace GlobalX.ChatBots.WebexTeams.Tests.Services
+{
+    public class WebexTeamsPersonHandlerTest
+    {
+        private string _input;
+        private GlobalXPerson _output;
+
+        private WebexTeamsPersonHandler _subject;
+
+        // mocks
+        private readonly IWebexTeamsApiService _apiService;
+
+        public WebexTeamsPersonHandlerTest()
+        {
+            _apiService = Substitute.For<IWebexTeamsApiService>();
+            var mapper = new MapperConfiguration(c =>
+            {
+                c.AddProfile<CommonMappers>();
+                c.AddProfile<PersonMapper>();
+            }).CreateMapper();
+            _subject = new WebexTeamsPersonHandler(_apiService, new WebexTeamsMapper(mapper));
+        }
+
+        [Theory]
+        [MemberData(nameof(WebexTeamsPersonHandlerTestData.SuccessfulGetPersonTestData), MemberType = typeof(WebexTeamsPersonHandlerTestData))]
+        internal void TestGetPerson(string input, WebexTeamsPerson apiResponse, GlobalXPerson output)
+        {
+            this.Given(x => GivenAPersonId(input))
+                .When(x => WhenGettingAPerson(apiResponse))
+                .Then(x => ThenItShouldReturnPersonDetails(output))
+                .BDDfy();
+        }
+
+        private void GivenAPersonId(string input)
+        {
+            _input = input;
+        }
+
+        private async void WhenGettingAPerson(WebexTeamsPerson apiResponse)
+        {
+            _apiService.GetPersonAsync(_input).Returns(Task.FromResult(apiResponse));
+            _output = await _subject.GetPersonAsync(_input);
+        }
+
+        private void ThenItShouldReturnPersonDetails(GlobalXPerson output)
+        {
+            _output.ShouldNotBeNull();
+            _output.ShouldSatisfyAllConditions(
+                () => _output.Created.ShouldBe(output.Created),
+                () => _output.Username.ShouldBe(output.Username),
+                () => _output.UserId.ShouldBe(output.UserId),
+                () =>
+                {
+                    if (output.Emails != null)
+                    {
+                        _output.Emails.ShouldNotBeNull();
+                        _output.Emails.OrderBy(x => x).SequenceEqual(output.Emails.OrderBy(x => x)).ShouldBe(true);
+                    }
+                    else
+                    {
+                        _output.Emails.ShouldBeNull();
+                    }
+                },
+                () => _output.Type.ShouldBe(output.Type)
+            );
+        }
+    }
+}
---
+++ src/GlobalX.ChatBots.WebexTeams.Tests/TestData/WebexTeamsPersonHandlerTestData.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using GlobalX.ChatBots.Core.People;
+using GlobalXPerson = GlobalX.ChatBots.Core.People.Person;
+using WebexTeamsPerson = GlobalX.ChatBots.WebexTeams.Models.Person;
+
+namespace GlobalX.ChatBots.WebexTeams.Tests.TestData
+{
+    public class WebexTeamsPersonHandlerTestData
+    {
+        public static IEnumerable<object[]> SuccessfulGetPersonTestData()
+        {
+            yield return new object[]
+            {
+                "personId",
+                new WebexTeamsPerson
+                {
+                    Id = "personId",
+                    Emails = new []{ "test.person@test.com" },
+                    DisplayName = "Test Person",
+                    NickName = "Testy",
+                    FirstName = "Test",
+                    LastName = "Person",
+                    Avatar = "https://fake-address.com",
+                    OrgId = "orgId",
+                    Created = DateTime.Parse("2018-08-12T22:17:24.697Z", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal),
+                    Status = "unknown",
+                    Type = "person"
+                },
+                new GlobalXPerson
+                {
+                    Created = DateTime.Parse("2018-08-12T22:17:24.697Z", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal),
+                    Username = "Test Person",
+                    UserId = "personId",
+                    Emails = new []{ "test.person@test.com" },
+                    Type = PersonType.Person
+                }
+            };
+
+            yield return new object[]
+            {
+                "botId",
+                new WebexTeamsPerson
+                {
+                    Id = "botId",
+                    Emails = new []{ "TestBot@webex.bot" },
+                    PhoneNumbers = new string[0],
+                    DisplayName = "TestBot",
+                    NickName = "TestBot",
+                    Avatar = "https://test-bot-avatar.com",
+                    OrgId = "orgId",
+                    Created = DateTime.Parse("2018-12-17T22:26:10.283Z", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal),
+                    Status = "unknown",
+                    Type = "bot"
+                },
+                new GlobalXPerson
+                {
+                    Created = DateTime.Parse("2018-12-17T22:26:10.283Z", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal),
+                    Username = "TestBot",
+                    UserId = "botId",
+                    Emails = new []{ "TestBot@webex.bot" },
+                    Type = PersonType.Bot
+                }
+            };
+        }
+    }
+}
