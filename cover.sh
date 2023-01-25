8c5b7e1a8a68eb8be24ef5bf7937cdfceeabe5e5
Laura Berger-Uichanco <laura.berger@globalx.com.au>
2023-01-25 15:48:25 +1000

    chore: update release files

--- cover.sh
+++ cover.sh
@@ -1,2 +1,9 @@
-dotnet test src/GlobalX.ChatBots.WebexTeams.Tests/GlobalX.ChatBots.WebexTeams.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
-bash <(curl -s https://codecov.io/bash) -f src/GlobalX.ChatBots.WebexTeams.Tests/coverage.opencover.xml
+#!/usr/bin/env bash
+
+docker run -v $(pwd)/src:/src mcr.microsoft.com/dotnet/sdk:6.0-alpine dotnet test src/GlobalX.ChatBots.WebexTeams.Tests/GlobalX.ChatBots.WebexTeams.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="opencover"
+curl -Os https://uploader.codecov.io/latest/linux/codecov
+chmod +x codecov
+./codecov -f src/PockyBot.NET.Tests/coverage.opencover.xml -t $CODECOV_TOKEN --sha $COMMIT_SHA
+
+# Codacy
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r src/GlobalX.ChatBots.WebexTeams.Tests/coverage.opencover.xml --commit-uuid $COMMIT_SHA
